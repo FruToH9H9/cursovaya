17286c9e145a9f26e8a85adb52bf1931626cbf2d
Nikita <nikita_moroz_2005@mail.ru>
2025-03-19 18:48:51 +0300

    разбил логику и отображение на 2 файла, но перестало работать корректное окрашивание

--- dispetcher.py
+++ dispetcher.py
@@ -1,7 +1,7 @@
 import psutil
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import Qt, QTimer
-from PyQt6.QtGui import QColor, QBrush
+from dispetcher_logic import *
 import sys
 import os
 
@@ -59,10 +59,6 @@ class MainWindow(QMainWindow):
         self.grid_layout.addWidget(self.table, 0, 0)
 
         #Для нужд
-        self.processes = {}
-        self.prev_cpu = {}  
-        self.prev_io = {} 
-        self.prev_net = {}
         self.selected_name = None
         
         #таймер
@@ -70,8 +66,6 @@ class MainWindow(QMainWindow):
         self.timer = QTimer()
         self.timer.timeout.connect(self.update_table)
         self.timer.start(self.timer_clock)
-
-        self.update_process_list()
         
     def restore_selection(self, name):
         for row in range(self.table.rowCount()):
@@ -79,17 +73,6 @@ class MainWindow(QMainWindow):
             if item and str(item.text()) == name:
                 self.table.selectRow(row)  
                 break
-        
-    def update_process_list(self):
-        self.processes = {}
-        for proc in psutil.process_iter(['pid', 'name']):
-            try:
-                if proc.info['name'] == "System Idle Process":
-                    continue
-                proc.cpu_percent(interval=0.0)  
-                self.processes[proc.pid] = proc
-            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-                continue
 
     def update_table(self):
         selected_items = self.table.selectedItems()
@@ -98,7 +81,8 @@ class MainWindow(QMainWindow):
         else:
             self.selected_name = None
             
-        info = self.info_zadachi()
+        logic = dispetcher_logic()
+        info = logic.info_zadachi()
         self.table.setRowCount(len(info))
         for i, row in enumerate(info):
             for j, item in enumerate(row):
@@ -112,119 +96,14 @@ class MainWindow(QMainWindow):
             self.restore_selection(self.selected_name)
                 
         self.table.resizeColumnsToContents()
-        
-    def info_zadachi(self):
-        process_dict = {}  # Словарь для группировки процессов по именам
-
-        for pid, proc in list(self.processes.items()):
-            try:
-                name = proc.info['name']
-                pid = proc.info['pid']
-                memory_info = proc.memory_info().rss / 1024**2 
-                cpu_percent = proc.cpu_percent(interval=0.0)  
-                cpu_percent = cpu_percent / psutil.cpu_count(logical=True) 
-
-                io_counters = proc.io_counters()
-                prev_read, prev_write = self.prev_io.get(pid, (io_counters.read_bytes, io_counters.write_bytes))
-                disk_usage = ((io_counters.read_bytes - prev_read) + (io_counters.write_bytes - prev_write)) / 1024**2  # МБ/с
-                self.prev_io[pid] = (io_counters.read_bytes, io_counters.write_bytes)
-                
-                net_counters = proc.io_counters()
-                prev_sent, prev_recv = self.prev_net.get(pid, (net_counters.other_bytes, net_counters.read_bytes))
-                net_speed = ((net_counters.other_bytes - prev_sent) + (net_counters.read_bytes - prev_recv)) * 8 / 10**6  # Мбит/с
-                self.prev_net[pid] = (net_counters.other_bytes, net_counters.read_bytes)
-                
-                power_usage = self.estimate_power_usage(cpu_percent, net_speed, memory_info)
-
-                if name in process_dict:
-                    process_dict[name]['cpu_percent'] += cpu_percent
-                    process_dict[name]['memory_info'] += memory_info
-                    process_dict[name]['disk_usage'] += disk_usage
-                    process_dict[name]['net_speed'] += net_speed
-                    process_dict[name]['power_usage'] = self.estimate_power_usage(
-                        process_dict[name]['cpu_percent'],
-                        process_dict[name]['net_speed'],
-                        process_dict[name]['memory_info']
-                    )
-                else:
-                    # Иначе добавляем новый процесс в словарь
-                    process_dict[name] = {
-                        'cpu_percent': cpu_percent,
-                        'memory_info': memory_info,
-                        'disk_usage': disk_usage,
-                        'net_speed': net_speed,
-                        'power_usage': power_usage
-                    }
-
-            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-                continue
-
-        # Преобразуем словарь обратно в список для отображения в таблице
-        result = []
-        for name, values in process_dict.items():
-            row = [
-                pid,
-                name,  # Имя процесса   
-                f"{values['cpu_percent']:.2f}%",  # Использование CPU в %
-                f"{values['memory_info']:.2f} MB",  # Использование памяти в МБ
-                f"{values['disk_usage']:.2f} MB/s",  # Скорость диска в МБ/с
-                f"{values['net_speed']:.2f} Mb/s",  # Скорость сети в Мбит/c
-                values['power_usage']  # Использование аккумулятора в статусе
-            ]
-            result.append(row)
-
-        return result
-
-    def estimate_power_usage(self, cpu, network, memory):
-        power_score = (cpu * 0.7) + (network * 0.1) + (memory * 0.1)
-        if power_score < 10:
-            return "Очень низкое"
-        elif power_score < 30:
-            return "Низкое"
-        elif power_score < 60:
-            return "Среднее"
-        elif power_score < 90:
-            return "Высокое"
-        else:
-            return "Очень высокое"
-
+    
     def set_cell_color(self, row, column, item):
         value = item.text()
-        if column == 2:  # ЦП
-            value = float(value.replace('%', ''))
-            if value > 50.0:
-                item.setBackground(QBrush(QColor(255, 0, 0)))  # Красный
-            elif value > 20.0:
-                item.setBackground(QBrush(QColor(255, 255, 0)))  # Желтый
-            elif value > 5.0:
-                item.setBackground(QBrush(QColor(0, 255, 0)))  # Зеленый
-                
-        elif column == 3:  # Память
-            value = float(value.replace(' MB', ''))
-            if value > 2000:
-                item.setBackground(QBrush(QColor(255, 0, 0)))  
-            elif value > 500:
-                item.setBackground(QBrush(QColor(255, 255, 0)))  
-            elif value > 100:
-                item.setBackground(QBrush(QColor(0, 255, 0)))  
-                  
-        elif column == 4:  # Диск (МБ/с)
-            value = float(value.replace(' MB/s', ''))
-            if value > 50:
-                item.setBackground(QBrush(QColor(255, 0, 0)))  
-            elif value > 10:
-                item.setBackground(QBrush(QColor(255, 255, 0)))  
-            elif value > 1:
-                item.setBackground(QBrush(QColor(0, 255, 0)))  
-                  
-        elif column == 5:  # Сеть (Мбит/с)
-            value = float(value.replace(' Mb/s', ''))
-            if value > 10:
-                item.setBackground(QBrush(QColor(255, 0, 0)))  
-            elif value > 1:
-                item.setBackground(QBrush(QColor(255, 255, 0)))  
-            elif value > 0.3:
-                item.setBackground(QBrush(QColor(0, 255, 0)))  
+        list_signs = ['%', ' MB', ' MB/s', ' Mb/s']
+        value_list = [5, 100, 1, 0.3] #Колонны: ЦП, Память, Диск, Сеть
+        color_logic = dispetcher_logic()
+        if 1 < column < 6:
+            color_logic.paint(value=value, compare_to=value_list[column-2], item=item, sign=list_signs[column-2])
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
---
+++ dispetcher_logic.py
@@ -0,0 +1,97 @@
+import psutil
+from PyQt6.QtGui import QColor, QBrush
+
+class dispetcher_logic():
+    def __init__(self):
+        self.prev_io = {} 
+        self.prev_net = {}
+    
+    def info_zadachi(self):
+        process_dict = {}  #словарь чтобы обьединять процессы с 1 именем
+        for proc in psutil.process_iter(['pid', 'name']):
+            try:
+                if proc.info['name'] == "System Idle Process": #скип процессов к которым нет доступа(они выдают странные данные)
+                    continue
+                
+                name = proc.info['name'] #имя процесса
+                pid = proc.info['pid'] #айди процесса
+                memory_info = proc.memory_info().rss / 1024**2  #память
+                cpu_percent = proc.cpu_percent(interval=0.0)  #ЦП
+                
+                #диск
+                """Логика диска заключена в том, что выдается разница между двумя вычислениями, если выдавать просто значение в данный
+                момент, то будет слишком огромное значение"""
+                io_counters = proc.io_counters()
+                prev_read = self.prev_io.get(pid, io_counters.read_bytes) #получаем предыдущее значение чтения по айди процесса
+                prev_write = self.prev_io.get(pid, io_counters.write_bytes) #записи
+                self.prev_io[pid] = (io_counters.read_bytes, io_counters.write_bytes) #сохраняем текущие
+                disk_usage = ((io_counters.read_bytes - prev_read) + (io_counters.write_bytes-prev_write)) / 1024**2  # МБ/с
+                
+                #сеть
+                net_counters = proc.io_counters()
+                prev_read_bytes = self.prev_net.get(pid, net_counters.read_bytes) #тут логика аналогична диску
+                prev_other_bytes = self.prev_net.get(pid, net_counters.other_bytes)
+                net_speed = ((net_counters.other_bytes - prev_other_bytes) + (net_counters.read_bytes - prev_read_bytes)) * 8 / 10**6  # Мбит/с
+                
+                #энергопотребление
+                power_usage = self.estimate_power_usage(cpu_percent, net_speed, memory_info)
+
+                if name in process_dict:
+                    process_dict[name]['cpu_percent'] += cpu_percent
+                    process_dict[name]['memory_info'] += memory_info
+                    process_dict[name]['disk_usage'] += disk_usage
+                    process_dict[name]['net_speed'] += net_speed
+                    process_dict[name]['power_usage'] = self.estimate_power_usage(
+                        process_dict[name]['cpu_percent'],
+                        process_dict[name]['net_speed'],
+                        process_dict[name]['memory_info']
+                    )
+                else:
+                    # Иначе добавляем новый процесс в словарь
+                    process_dict[name] = {
+                        'cpu_percent': cpu_percent,
+                        'memory_info': memory_info,
+                        'disk_usage': disk_usage,
+                        'net_speed': net_speed,
+                        'power_usage': power_usage
+                    }
+
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                continue
+
+        result = []
+        for name, values in process_dict.items():
+            row = [
+                pid,
+                name,  # Имя процесса   
+                f"{values['cpu_percent']:.2f}%",  # Использование CPU в %
+                f"{values['memory_info']:.2f} MB",  # Использование памяти в МБ
+                f"{values['disk_usage']:.2f} MB/s",  # Скорость диска в МБ/с
+                f"{values['net_speed']:.2f} Mb/s",  # Скорость сети в Мбит/c
+                values['power_usage']  # Использование аккумулятора в статусе
+            ]
+            result.append(row)
+
+        return result
+    
+    def estimate_power_usage(self, cpu, network, memory):
+        power_score = (cpu * 0.7) + (network * 0.1) + (memory * 0.1)
+        if power_score < 10:
+            return "Очень низкое"
+        elif power_score < 30:
+            return "Низкое"
+        elif power_score < 60:
+            return "Среднее"
+        elif power_score < 90:
+            return "Высокое"
+        else:
+            return "Очень высокое"
+        
+    def paint(self, value: int, compare_to: int, item: str, sign: str):
+        value = float(value.replace(sign, ''))
+        if value > compare_to:
+            return item.setBackground(QBrush(QColor('light green')))
+        elif value > compare_to*4:  
+            return item.setBackground(QBrush(QColor('yellow')))
+        elif value > compare_to*16:  
+            return item.setBackground(QBrush(QColor('red')))
